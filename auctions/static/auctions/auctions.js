aeaf879bc9a046ddab2155cbe9aaea2c6063ff9a
Marshall Pratt <marshallpratt1@yahoo.com>
2022-11-01 08:41:40 -0800

    added lightbox

--- auctions/static/auctions/auctions.js
+++ auctions/static/auctions/auctions.js
@@ -1,20 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     //update_stats();
     //setInterval(update_stats, 2000);
-    //document.querySelector('#watchlist_form').onclick = watchlist
     });
     
-
-const watch = document.getElementById('watch')
-watch.addEventListener(click, (click)=>{
-        
-        //console.log(listing_id)
-        alert("This alert box was called!!!");
-       
-        //fetch("/api/watchlist")
-
-        return false;
-    });
 
 function watchlist() {
     if (document.querySelector('#watch').src == "https://cdn-icons-png.flaticon.com/512/1077/1077035.png"){
@@ -23,17 +11,10 @@ function watchlist() {
 else {
     document.querySelector('#watch').src = "https://cdn-icons-png.flaticon.com/512/1077/1077035.png";
 }
-    /*
-    if (document.querySelector('#watch').style.backgroundColor == "black"){
-        document.querySelector('#watch').style.backgroundColor = "transparent";
-    }
-    else {
-        document.querySelector('#watch').style.backgroundColor = "black";
-    }
-    */
-}
+};
 
-    function update_stats() {  
+
+function update_stats() {  
         fetch("/api/status")
         .then(response => response.json())
         .then(data => {
